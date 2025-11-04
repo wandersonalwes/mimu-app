08e36e834f0e4139254dce63b66ccd353cbbe31a
Wanderson Alves <wandersonalwes@hotmail.com>
2025-11-04 12:17:50 -0300

    feat: integrate NativeWind for styling and add global CSS support
    - Added metro.config.js to configure Metro bundler with NativeWind.
    - Created nativewind-env.d.ts for TypeScript support with NativeWind.
    - Updated package.json to include NativeWind and related dependencies.
    - Added postcss.config.mjs for PostCSS configuration with Tailwind CSS.
    - Modified _layout.tsx to import global CSS styles.
    - Updated index.tsx to use Tailwind CSS classes for styling.
    - Created global.css for Tailwind CSS and NativeWind theme definitions.
    - Updated tsconfig.json to include nativewind-env.d.ts for TypeScript.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,3 +1,5 @@
+import '@/styles/global.css'
+
 import { DefaultTheme, ThemeProvider } from '@react-navigation/native'
 import { useFonts } from 'expo-font'
 import { Stack } from 'expo-router'
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from 'react-native'
 
 export default function HomeScreen() {
   return (
-    <View style={{ flex: 1 }}>
-      <Text style={{ fontSize: 24, fontFamily: 'ManropeBold' }}>Mimu</Text>
+    <View className="flex-1 bg-background justify-center items-center">
+      <Text className="text-2xl text-foreground font-manrope-bold">Mimu</Text>
     </View>
   )
 }
