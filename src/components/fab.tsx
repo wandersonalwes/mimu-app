035e5c3759f1b9032be1b7db6625f568fc6816da
Wanderson Alves <wandersonalwes@hotmail.com>
2025-11-10 20:57:39 -0300

    feat: refactor Fab component to use style props and improve responsiveness

--- src/components/fab.tsx
+++ src/components/fab.tsx
@@ -1,14 +1,27 @@
-import { cn } from '@/libs/cn'
-import { TouchableOpacity, TouchableOpacityProps } from 'react-native'
+import { useTheme } from '@react-navigation/native'
+import { StyleSheet, TouchableOpacity, TouchableOpacityProps } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 export function Fab({ className, ...props }: TouchableOpacityProps) {
+  const { colors } = useTheme()
+  const { bottom } = useSafeAreaInsets()
   return (
     <TouchableOpacity
-      className={cn(
-        'size-16 rounded-full bg-primary dark:bg-primary-dark justify-center items-center absolute bottom-8 right-5',
-        className
-      )}
+      style={[styles.fab, { bottom: bottom + 16, backgroundColor: colors.primary }]}
       {...props}
     />
   )
 }
+
+const styles = StyleSheet.create({
+  fab: {
+    position: 'absolute',
+    right: 20,
+    bottom: 20,
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
