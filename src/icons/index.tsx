95bd66d86c68cefb258cf08a1d8c7fa3cabe425a
Wanderson Alves <wandersonalwes@hotmail.com>
2025-11-13 12:26:07 -0300

    refactor: simplify icon imports by using namespace import from phosphor-react-native

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -1,25 +1,8 @@
 import { MimuLogo } from '@/components/mimu-logo'
 import { styled } from 'nativewind'
 
-import {
-  ArrowLeftIcon as ArrowLeft,
-  BooksIcon as Books,
-  CardsIcon as Cards,
-  CaretRightIcon as CaretRight,
-  CheckIcon as Check,
-  DotsThreeIcon as DotsThree,
-  FunnelIcon as Funnel,
-  GearSixIcon as GearSix,
-  HeartIcon as Heart,
-  IconProps,
-  PencilIcon as Pencil,
-  PlusIcon as Plus,
-  PuzzlePieceIcon as PuzzlePiece,
-  SealQuestionIcon as SealQuestion,
-  SpeakerHighIcon as SpeakerHigh,
-  TargetIcon as Target,
-  TrashIcon as Trash,
-} from 'phosphor-react-native'
+import type { IconProps } from 'phosphor-react-native'
+import * as PhosphorIcons from 'phosphor-react-native'
 import { View } from 'react-native'
 
 // --- Helper function to create styled icon components ---
@@ -37,22 +20,22 @@ function createStyledIcon(icon: React.ComponentType<IconProps>) {
 export const MimuIcon = styled(MimuLogo, { className: 'style' })
 
 // --- Phosphor Icons ---
-export const ArrowLeftIcon = createStyledIcon(ArrowLeft)
-export const PlusIcon = createStyledIcon(Plus)
-export const GearSixIcon = createStyledIcon(GearSix)
-export const CaretRightIcon = createStyledIcon(CaretRight)
-export const CheckIcon = createStyledIcon(Check)
-export const CardsIcon = createStyledIcon(Cards)
-export const BooksIcon = createStyledIcon(Books)
-export const SealQuestionIcon = createStyledIcon(SealQuestion)
-export const TargetIcon = createStyledIcon(Target)
-export const PuzzlePieceIcon = createStyledIcon(PuzzlePiece)
-export const FunnelIcon = createStyledIcon(Funnel)
-export const SpeakerHighIcon = createStyledIcon(SpeakerHigh)
-export const HeartIcon = createStyledIcon(Heart)
-export const TrashIcon = createStyledIcon(Trash)
-export const PencilIcon = createStyledIcon(Pencil)
-export const DotsThreeIcon = createStyledIcon(DotsThree)
+export const ArrowLeftIcon = createStyledIcon(PhosphorIcons.ArrowLeftIcon)
+export const PlusIcon = createStyledIcon(PhosphorIcons.PlusIcon)
+export const GearSixIcon = createStyledIcon(PhosphorIcons.GearSixIcon)
+export const CaretRightIcon = createStyledIcon(PhosphorIcons.CaretRightIcon)
+export const CheckIcon = createStyledIcon(PhosphorIcons.CheckIcon)
+export const CardsIcon = createStyledIcon(PhosphorIcons.CardsIcon)
+export const BooksIcon = createStyledIcon(PhosphorIcons.BooksIcon)
+export const SealQuestionIcon = createStyledIcon(PhosphorIcons.SealQuestionIcon)
+export const TargetIcon = createStyledIcon(PhosphorIcons.TargetIcon)
+export const PuzzlePieceIcon = createStyledIcon(PhosphorIcons.PuzzlePieceIcon)
+export const FunnelIcon = createStyledIcon(PhosphorIcons.FunnelIcon)
+export const SpeakerHighIcon = createStyledIcon(PhosphorIcons.SpeakerHighIcon)
+export const HeartIcon = createStyledIcon(PhosphorIcons.HeartIcon)
+export const TrashIcon = createStyledIcon(PhosphorIcons.TrashIcon)
+export const PencilIcon = createStyledIcon(PhosphorIcons.PencilIcon)
+export const DotsThreeIcon = createStyledIcon(PhosphorIcons.DotsThreeIcon)
 
 // --- Countries ---
 export { BrazilIcon } from './countries/brazil'
